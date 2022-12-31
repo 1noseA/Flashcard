70096ff88ffd974e889650e1a2cda0c4737c9f30
1noseA <1noseA2L@gmail.com>
2022-12-31 11:52:48 +0900

    シードデータ追加

--- Flashcard/Data/FlashcardContext.cs
+++ Flashcard/Data/FlashcardContext.cs
@@ -41,6 +41,22 @@ namespace Flashcard.Data
                 .HasOne(histories => histories.Words)
                 .WithMany(words => words.Histories)
                 .HasForeignKey(histories => histories.WordId);
+
+            // seed data
+            modelBuilder.Entity<Users>().HasData(new Users { UserId = 1, UserName = "user1", Password="password1" });
+            modelBuilder.Entity<Users>().HasData(new Users { UserId = 2, UserName = "user2", Password = "password2" });
+
+            modelBuilder.Entity<Words>().HasData(
+                new Words { UserId = 1, WordId = 1, Word = "word1", Meaning = "meaning1" });
+            modelBuilder.Entity<Words>().HasData(
+                new Words { UserId = 1, WordId = 2, Word = "word2", Meaning = "meaning2" });
+            modelBuilder.Entity<Words>().HasData(
+                new Words { UserId = 1, WordId = 3, Word = "word3", Meaning = "meaning3" });
+            modelBuilder.Entity<Words>().HasData(
+                new Words { UserId = 1, WordId = 4, Word = "word4", Meaning = "meaning4" });
+            modelBuilder.Entity<Words>().HasData(
+                new Words { UserId = 1, WordId = 5, Word = "word5", Meaning = "meaning5" });
+
         }
     }
 }
