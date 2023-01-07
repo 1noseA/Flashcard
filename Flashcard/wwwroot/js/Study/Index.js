1acf4adcb63adb2b547460d38c7417a12751368b
1noseA <1noseA2L@gmail.com>
2023-01-07 12:32:18 +0900

    判定時の背景色変更

---
+++ Flashcard/wwwroot/js/Study/Index.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    let judgement = document.getElementById('judgement').textContent;
+    if (judgement === '〇') {
+        $('.myAnswer').addClass('bg-green');
+    } else if (judgement === '×') {
+        $('.myAnswer').addClass('bg-red');
+    }
+});
